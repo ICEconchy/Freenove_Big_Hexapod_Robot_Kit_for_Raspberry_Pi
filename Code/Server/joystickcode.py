ded2f6cab4208436a6414aa056724aa39a666c9b
ICEconchy <j.j.trevis-smith@blueyonder.co.uk>
2021-09-05 22:35:24 +0100

    Started analog event decrypt

--- Code/Server/joystickcode.py
+++ Code/Server/joystickcode.py
@@ -1,8 +1,7 @@
 #Import everything in the control module, 
 #including functions, classes, variables, and more.
 from Control import *
-from evdev import InputDevice, categorize, ecodes
-
+from evdev import list_devices, InputDevice, categorize, ecodes, KeyEvent
 
 #Creating object 'control' of 'Control' class.
 c=Control()
@@ -15,19 +14,19 @@ c.order=['','','','','','']
 #Delay:'10'
 #Action Mode : '0'   Angleless turn 
 
-#creates object 'gamepad' to store the data
-#you can call it whatever you like
-gamepad = InputDevice('/dev/input/event0')
-
-#button code variables (change to suit your device)
+gamepad = InputDevice('/dev/input/event0') #creates object 'gamepad' to store the data
+#button codes
 aBtn = 304
 bBtn = 305
 xBtn = 307
 yBtn = 308
 LB = 310
 RB = 311
-
-zpos = 10
+Powbtn = 316
+#AnalR =
+#AnalL = 
+CENTER_TOLERANCE = 350
+zpos = 0 # initial position
 #prints out device info at start
 print(gamepad)
 c.order=['','','','','',''] #data to be sent to control
@@ -57,14 +56,47 @@ for event in gamepad.read_loop():
                     c.order=['CMD_MOVE', '1', '-35', '0', '10', '0']
                     c.condition()
                     print('X Button Pressed')
+            elif event.code == PowBtn
+                c.order=['CMD_BUZZER', '1']
+                c.condition()
+                sleep(2)
+                c.order=['CMD_BUZZER', '1']
+                c.condition
             elif event.code == LB:
-                for i in range(1):
-                    c.order=['CMD_POSITION','0','0','-10']
-                    c.condition()
-                    print('LB Button Pressed')
+                if zpos < -19:
+                    zpos = -20
+                else:
+                    zpos = zpos -2 
+                c.order=['CMD_POSITION','0','0', zpos]
+            #    print(zpos)
+                c.condition()
+            #    print('LB Button Pressed')
             elif event.code == RB:
-               # for i in range(1):
-                 #   zpos = zpos +1
-                    c.order=['CMD_POSITION','0','0','11']
-                    c.condition()
-                    print('RB Button Pressed')
+                if zpos > 19: 
+                    zpos = 20
+                else:
+                    zpos = zpos +2
+                c.order=['CMD_POSITION','0','0', zpos]
+           #     print(zpos)
+                c.condition()
+           #     print('RB Button Pressed')
+                
+#for event in gamepad.read_loop():
+    if event.type == ecodes.EV_ABS: 
+        absevent = categorize(event)
+        print('I got here ',event)
+        if ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_RZ':
+           translation = {97: None, 98: None, 99: 105}
+           if absevent.event.value > 128: 
+                print ('reverse') 
+                print (absevent.event.value) 
+            elif absevent.event.value < 127:
+                print ('forward') 
+                print (absevent.event.value) 
+    if ecodes.bytype[absevent.event.type][absevent.event.code] == 'ABS_Z':
+            if absevent.event.value > 128 : 
+                print ('right') 
+                print (absevent.event.value) 
+            elif absevent.event.value < 127: 
+                print ('left') 
+                print (absevent.event.value)
